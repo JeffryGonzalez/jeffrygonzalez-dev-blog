9404894aaf5b37372630b3505b45a19d5a2cdccc
Jeffry Gonzalez <jeff@hypertheory.com>
2026-04-01 18:01:43 -0400

    update

--- astro.config.ts
+++ astro.config.ts
@@ -32,11 +32,10 @@ export default defineConfig({
   image: {
     // Used for all Markdown images; not configurable per-image
     // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-   // experimentalLayout: "responsive",
+   // layout: "responsive",
   },
   experimental: {
     //svg: true,
-    responsiveImages: true,
-    preserveScriptOrder: true,
+   // responsiveImages: true,
   },
 });
